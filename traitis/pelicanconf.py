f14898fd00ffb0bcf020224c102788db08180adc
Joshua McKenty <jmckenty@gmail.com>
2020-09-04 15:04:15 -0700

    Added theme

--- traitis/pelicanconf.py
+++ traitis/pelicanconf.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Protected Trait Data Trust'
 SITENAME = 'Traitis Data Trust'
-SITEURL = ''
+SITEURL = 'https://trait.is'
+THEME = "/var/www/traitis/themes/blue-penguin"
 
 PATH = 'content'
 
@@ -20,15 +21,28 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 PLUGINS=['plugins.sitemap']  # , 'plugins.gzip_cache']
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('GitHub', 'https://github.com/'),
+    ('Linux Kernel', 'https://www.kernel.org/'),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
